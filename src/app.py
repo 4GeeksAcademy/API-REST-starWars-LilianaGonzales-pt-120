eec680e2b335ded34cbbd3a47d7ee62cfa0c0a10
Liliana Gonzales Chavez <liligh14@gmail.com>
2025-12-08 19:01:31 +0000

    avance3

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Character, Vehicle, user_character
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,14 +28,19 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
@@ -44,6 +50,120 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+# @app.route('/users', methods=["POST"])
+# def create_user():
+#     data = request.get_json()
+#     user = User(
+#         username= data.get('username'),
+#         email = data.get('email'),
+#         password = data.get('password'),
+#         subscription = data.get('subscription')# va si el tipo de dato el solo DateTime
+#     )
+#     db.session.add(user)
+#     db.session.commit()
+
+#     print(data)
+#     return "ok", 200
+
+@app.route('/users', methods=["GET"])
+def get_users():
+    users = list(db.session.execute(db.select(User)).scalars())
+    print(users)
+    result = [user.to_dict() for user in users]
+    print(result)
+    return jsonify(result)
+
+
+@app.route('/users/<int:id>', methods=["GET"])
+def get_user(id):
+    user = db.session.get(User, id)
+    print(user.to_dict())
+    return jsonify(user.to_dict()), 200
+
+
+@app.route('/users', methods=["POST"])
+def create_user():
+    data = request.get_json()
+    user = User.create(
+        username=data.get('username'),
+        email=data.get('email'),
+        password=data.get('password'))
+    print(user)
+    return jsonify(user.to_dict())
+
+
+# character
+
+@app.route('/characters/<int:id>', methods=['GET'])
+def get_character(id):
+    character = db.session.get(Character, id)
+    print(character.list_vehicle)
+    # return jsonify(character.to_dict()), 200
+    return jsonify(character.to_dict_all_data()), 200
+
+
+@app.route('/characters', methods=["POST"])
+def create_character():
+    data = request.get_json()
+    character = Character.create(
+        name=data.get('name'),
+        gender=data.get('gender'),
+        height=data.get('height'))
+    print(character)
+    return jsonify(character.to_dict())
+
+# vehicles
+
+
+@app.route('/vehicles', methods=["POST"])
+def create_vehicle():
+    data = request.get_json()
+    vehicle = Vehicle(
+        name=data.get('name'),
+        model=data.get('model'),
+        passengers=data.get('passengers'),
+        character_id=data.get('character_id')
+    )
+    db.session.add(vehicle)
+    db.session.commit()
+    print(vehicle)
+    return 'ok', 200
+
+ # user_character
+
+
+# @app.route('/users/<int:user_id>/characters/<int:character_id>', methods=["POST"])
+@app.route('/users_characters', methods=["POST"])
+# def add_user_character(user_id, character_id):
+def add_user_character():
+    data = request.get_json()
+    print(data)
+    userCharacter = user_character.insert().values(
+        # userCharacter = user_character(
+        user_id=data.get('user_id'),
+        character_id=data.get('character_id'),
+        favorite=data.get('favorite')
+    )
+    db.session.add(userCharacter)
+    db.session.commit()
+    print(userCharacter)
+    return 'ok', 200
+
+# def add_character_to_user(user_id, character_id):
+#     favorite = request.json.get("favorite", False)
+
+#     insert_stmt = user_character.insert().values(
+#         user_id=user_id,
+#         character_id=character_id,
+#         favorite=favorite
+#     )
+#     db.session.execute(insert_stmt)
+#     db.session.commit()
+
+#     return jsonify({"msg": "Character added to user"}), 201
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer,DateTime
+from sqlalchemy import String, Boolean, Integer, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import List
 
 db = SQLAlchemy()
@@ -21,79 +21,154 @@ db = SQLAlchemy()
 #         }
 
 
-
-
 user_character = db.Table('users_characters',
-    db.Column('id', db.Integer, primary_key = True),
-    db.Column('user_id', db.Integer,db.ForeignKey("users.id"), nullable=False),
-    db.Column('character_id', db.Integer,db.ForeignKey("characters.id"), nullable=False),
-    db.Column('favorite', db.Boolean(), default=False)
-)
+                          db.Column('id', db.Integer, primary_key=True),
+                          db.Column('user_id', db.Integer, db.ForeignKey(
+                              "users.id"), nullable=False),
+                          db.Column('character_id', db.Integer, db.ForeignKey(
+                              "characters.id"), nullable=False),
+                          db.Column('favorite', db.Boolean(), default=False)
+                          )
 
 user_vehicle = db.Table('users_vehicles',
-    db.Column('id', db.Integer, primary_key = True),
-    db.Column('user_id', db.Integer,db.ForeignKey("users.id"), nullable=False),
-    db.Column('vehicle_id', db.Integer,db.ForeignKey("vehicles.id"), nullable=False),
-    db.Column('favorite', db.Boolean(), default=False)
-)
+                        db.Column('id', db.Integer, primary_key=True),
+                        db.Column('user_id', db.Integer, db.ForeignKey(
+                            "users.id"), nullable=False),
+                        db.Column('vehicle_id', db.Integer, db.ForeignKey(
+                            "vehicles.id"), nullable=False),
+                        db.Column('favorite', db.Boolean(), default=False)
+                        )
 
 user_planet = db.Table('users_planets',
-    db.Column('id', db.Integer, primary_key = True),
-    db.Column('user_id', db.Integer,db.ForeignKey("users.id"), nullable=False),
-    db.Column('planet_id', db.Integer,db.ForeignKey("planets.id"), nullable=False),
-    db.Column('favorite', db.Boolean(), default=False)
-)
+                       db.Column('id', db.Integer, primary_key=True),
+                       db.Column('user_id', db.Integer, db.ForeignKey(
+                           "users.id"), nullable=False),
+                       db.Column('planet_id', db.Integer, db.ForeignKey(
+                           "planets.id"), nullable=False),
+                       db.Column('favorite', db.Boolean(), default=False)
+                       )
+
 
 class User(db.Model):
     __tablename__ = 'users'
 
-    id:Mapped[int] = mapped_column(Integer, primary_key= True)
-    username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(
+        String(80), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(20), nullable=False)
-    subscription: Mapped[datetime] = mapped_column(DateTime)
+    # subscription: Mapped[datetime] = mapped_column(DateTime,nullable=False)
+    subscription: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(timezone.utc))
 
-    characteres: Mapped[List["Character"]] = relationship(back_populates="users", secondary=user_character)
-    vehicles:  Mapped[List["Vehicle"]] = relationship(back_populates="users", secondary=user_vehicle)
-    planets: Mapped[List["Planet"]] = relationship(back_populates="users", secondary=user_planet)
+    characteres: Mapped[List["Character"]] = relationship(
+        back_populates="users", secondary=user_character)
+    vehicles:  Mapped[List["Vehicle"]] = relationship(
+        back_populates="users", secondary=user_vehicle)
+    planets: Mapped[List["Planet"]] = relationship(
+        back_populates="users", secondary=user_planet)
+
+    @classmethod
+    def create(self, username, email, password):
+        user = self(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
+    
+    def to_dict_all_data(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'characteres':[character.to_dict() for character in self.characteres]
+        }
+
 
 class Character (db.Model):
-    __tablename__= 'characters'
+    __tablename__ = 'characters'
 
-    id:Mapped[int] = mapped_column(Integer, primary_key= True)
-    name: Mapped[str] = mapped_column(String(80),nullable=False)
-    gender: Mapped[str] = mapped_column(String(20),nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(80), nullable=False)
+    gender: Mapped[str] = mapped_column(String(20), nullable=False)
     height: Mapped[int] = mapped_column(Integer, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime)
+    # created_at: Mapped[datetime] = mapped_column(DateTime)
+    created_at: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(timezone.utc))
 
-    list_vehicle : Mapped [List["Vehicle"]] = relationship(back_populates="character")
+    list_vehicle: Mapped[List["Vehicle"]] = relationship(
+        back_populates="character")
 
-    users: Mapped[List["User"]] = relationship(back_populates="characteres", secondary=user_character )
+    users: Mapped[List["User"]] = relationship(
+        back_populates="characteres", secondary=user_character)
+
+    @classmethod
+    def create(self, name, gender, height):
+        character = self(name=name, gender=gender, height=height)
+        db.session.add(character)
+        db.session.commit()
+        return character
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'gender': self.gender,
+            'height': self.height
+        }
+    
+    def to_dict_all_data(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'gender': self.gender,
+            'height': self.height,
+            'list_vehicle':[vehicle.to_dict() for vehicle in self.list_vehicle]
+        }
+
 
 class Planet (db.Model):
-    __tablename__= 'planets'
+    __tablename__ = 'planets'
 
-    id:Mapped[int] = mapped_column(Integer, primary_key= True)
-    name: Mapped[str] = mapped_column(String(50),nullable=False)
-    climate: Mapped[str] = mapped_column(String(20),nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    climate: Mapped[str] = mapped_column(String(20), nullable=False)
     population: Mapped[int] = mapped_column(Integer, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime)
+    created_at: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(timezone.utc))
 
-    users: Mapped[List["User"]] = relationship(back_populates="planets", secondary=user_planet)
+    users: Mapped[List["User"]] = relationship(
+        back_populates="planets", secondary=user_planet)
+
 
 class Vehicle (db.Model):
-    __tablename__= 'vehicles'
+    __tablename__ = 'vehicles'
 
-    id:Mapped[int] = mapped_column(Integer, primary_key= True)
-    name: Mapped[str] = mapped_column(String(50),nullable=False)
-    model: Mapped[str] = mapped_column(String(20),nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    model: Mapped[str] = mapped_column(String(20), nullable=False)
     passengers: Mapped[int] = mapped_column(Integer, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime)
+    created_at: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(timezone.utc))
 
-    character_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("characters.id"), nullable=False)
+    character_id: Mapped[int] = mapped_column(
+        Integer, db.ForeignKey("characters.id"), nullable=False)
 
-    character: Mapped["Character"] = relationship(back_populates="list_vehicle")
-    users: Mapped[List["User"]] = relationship(back_populates="vehicles", secondary=user_vehicle)
-
-
-
+    character: Mapped["Character"] = relationship(
+        back_populates="list_vehicle")
+    users: Mapped[List["User"]] = relationship(
+        back_populates="vehicles", secondary=user_vehicle)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'model': self.model,
+            'passengers': self.passengers
+        }
